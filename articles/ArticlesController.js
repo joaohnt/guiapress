b4bb52d0993266ebc41d343cc903fb498ff596aa
joaohnt <jpedrooo.cg@gmail.com>
2025-07-03 18:53:35 -0300

    mt coisa

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,123 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const slugify = require("slugify");
+const Article = require("./Article");
 
-router.get("/articles", (req, res) => {
-  res.send("articles List");
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({ include: [{ model: Category }] }).then((articles) => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.send("Create New article");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
+});
+
+router.post("/articles/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      // se n for numero
+      res.redirect("/admin/articles");
+    }
+  } else {
+    // null
+    res.redirect("/admin/articles");
+  }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (isNaN(id)) {
+    // se n for numero
+    res.redirect("/admin/articles");
+    return;
+  }
+
+  Article.findByPk(id) // findByPk busca pelo id primário
+    .then((article) => {
+      if (article != undefined) {
+        res.render("admin/articles/edit", { article: article });
+      } else {
+        res.redirect("/admin/articles");
+      }
+    })
+    .catch((err) => {
+      // erro ao buscar categoria
+      res.redirect("/admin/articles");
+    });
+});
+router.post("/articles/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+
+  Article.update(
+    { title: title, slug: slugify(title), body: body },
+    { where: { id: id } }
+  ).then(() => {
+    res.redirect("/admin/articles");
+  });
+});
+
+router.get("/articles/page/:num", (req, res) => {
+  var page = req.params.num;
+  var offset = 0;
+  if (isNaN(page) || page == 1) {
+    offset = 0;
+  } else {
+    offset = parseInt(page) * 4 - 1; // 4 artigos por página
+  }
+  Article.findAndCountAll({
+    limit: 4,
+    offset: offset,
+    order: [["id", "DESC"]],
+  }).then((articles) => {
+    var next = true;
+    if (offset + 4 >= articles.count) {
+      next = false; // n ha proxima paina
+    }
+
+    var result = {
+      page: parseInt(page),
+      articles: articles,
+      next: next,
+    };
+
+    Category.findAll().then((categories) => {
+      res.render("admin/articles/page", {
+        result: result,
+        categories: categories,
+      });
+    });
+  });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const conn = new Sequelize("guiapress", "root", "12345", {
   host: "localhost",
   dialect: "mysql", 
+  timezone: "-03:00",
 });
 
 module.exports = conn;
